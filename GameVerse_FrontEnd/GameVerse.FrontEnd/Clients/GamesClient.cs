0cf0cf51dafc27b868a60dd723f1f75099ada4b2
Ana Júlia Oliveira <anajulia.oliveyra@gmail.com>
2025-11-12 13:51:10 -0300

    Criação do projeto

---
+++ GameVerse_FrontEnd/GameVerse.FrontEnd/Clients/GamesClient.cs
@@ -0,0 +1,53 @@
+using GameVerse.FrontEnd.Models;
+
+namespace GameVerse.FrontEnd.Clients;
+
+public class GamesClient
+{
+    private readonly List<GameSummary> games =
+    [
+    new(){
+        Id=1,
+        Name="Kirby’s Return to Dream Land",
+        Genre = "Platform",
+        Price = 349.00M,
+        ReleaseDate = new DateOnly(2023,02,24)
+
+    },
+    new(){
+        Id=2,
+        Name="Stardew Valley",
+        Genre = "Rural Simulator",
+        Price = 24.99M,
+        ReleaseDate = new DateOnly(2016,02,26)
+
+    },
+    new(){
+        Id = 3,
+        Name = "Red Dead Redemption 2",
+        Genre = "Open World",
+        Price= 299.90M,
+        ReleaseDate = new DateOnly(2019,12,05)
+    },
+    new(){
+        Id = 4,Genre= "Sports",Name ="EA SPORTS FC™ 26",Price = 299.90M,ReleaseDate = new DateOnly(2025,09,25)
+    },
+    new(){
+         Id = 5,Genre= "Open World",Name ="Tunic",Price = 49.49M,ReleaseDate = new DateOnly(2022,03,16)
+    },
+    new(){
+         Id = 6,Genre= "Adventure",Name ="Hello Kitty Island Adventure",Price = 110.58M,ReleaseDate = new DateOnly(2025,01,30)
+    },
+    new(){
+        Id=7,Genre="Adventure",Name="Sonic Frontiers", Price=57.80M,ReleaseDate = new DateOnly(2022,11,08)
+    },
+    new(){
+        Id=8,Genre="Terror",Name="Phasmophobia",Price=59.99M,ReleaseDate=new DateOnly(2020,09,18)
+    },
+    new(){
+        Id=9,Genre="Puzzle",Name="Buckshot Roulette",Price=9.99M,ReleaseDate=new DateOnly(2024,04,04)
+    }
+    ];
+
+    public GameSummary[] GetGames() => games.ToArray();
+}
